c05580ca0972ded8b20282e3b8a4dee0b972af81
An-AvG-Geek <107713717+An-AvG-Geek@users.noreply.github.com>
2023-09-19 15:05:50 +0530

    Add files via upload
    This Java program prints hello world .
    This is the first program that is done in class.

---
+++ Hellojava.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+ * File         :			Hellojava.java
+ * Description  : 			My first java program that prints 
+ * 							"Hello Java"
+ * Author       : 			Felix Jobi
+ * Version      :			1.0
+ * Date         : 			19/09/23
+ ***************************************************************************************/
+
+
+
+
+
+package java_s3_062;
+
+public class Hellojava {
+	public static void main(String [] args) {
+		System.out.print("hello world");
+		System.out.println("hello fahad");
+	
+}
+}
