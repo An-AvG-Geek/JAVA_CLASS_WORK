af1060c6d9e6dbcc20b9b5706ffba574acffeac6
An-AvG-Geek <107713717+An-AvG-Geek@users.noreply.github.com>
2023-09-19 16:20:01 +0530

    Add files via upload

---
+++ DIV.java
@@ -0,0 +1,9 @@
+package java_s3_062;
+
+public class DIV {public static void main(String [] args) {
+	float num1=10,num2=5;
+	 float div=num2/num1;
+	 System.out.print("quotient is "+div);
+
+}
+}
---
+++ Mul.java
@@ -0,0 +1,9 @@
+package java_s3_062;
+
+public class Mul {
+	public static void main(String [] args) {
+		int num1=10,num2=20;
+		 int pro=num1*num2;
+		 System.out.print("product is "+pro);
+}
+}
