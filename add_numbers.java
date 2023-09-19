11e8a87f0fe8bccc33800345e45b89ecd6772ba0
An-AvG-Geek <107713717+An-AvG-Geek@users.noreply.github.com>
2023-09-19 16:15:16 +0530

    Add files via upload
    This java program add two numbers which are predefined in the program.

---
+++ add_numbers.java
@@ -0,0 +1,10 @@
+package java_s3_062;
+
+public class add_numbers {
+	public static void main(String [] args) {
+		int num1=10,num2=20;
+		 int sum=num1+num2;
+		 System.out.print("sum is "+sum);
+	}
+
+}
