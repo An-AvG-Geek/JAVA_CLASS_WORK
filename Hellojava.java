d94281cce2309f182721edcf850cac9fe905cc4b
An-AvG-Geek <107713717+An-AvG-Geek@users.noreply.github.com>
2023-09-19 16:17:55 +0530

    Update Hellojava.java

--- Hellojava.java
+++ Hellojava.java
@@ -1,7 +1,6 @@
 /******************************************************************************
  * File         :			Hellojava.java
- * Description  : 			This program add two numbers which are predefined 
- 					in the program 
+ * Description  : 			This program prints "hello World"
  * Author       : 			Felix Jobi
  * Version      :			1.0
  * Date         : 			19/09/23
@@ -16,7 +15,6 @@ package java_s3_062;
 public class Hellojava {
 	public static void main(String [] args) {
 		System.out.print("hello world");
-		System.out.println("hello fahad");
 	
 }
 }
