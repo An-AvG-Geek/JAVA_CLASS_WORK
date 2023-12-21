ebe7e2cb030cc03a5952cc61a4c9cc5aba8b27f5
Felix Jobi <107713717+An-AvG-Geek@users.noreply.github.com>
2023-12-21 15:40:44 +0000

    Add files via upload

---
+++ StringTokensDemo.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class StringTokensDemo {
+    public static void main(String args []) {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.print("Enter an Integer String:");
+    	String numbers = sc.nextLine();
+    	StringTokenizer input = new StringTokenizer(numbers);
+    	System.out.println("No of tokens before is "+input.countTokens());
+    	int sum=0;
+    	System.out.println("Integers are:");
+    	while(input.hasMoreElements()) {
+    		int num = Integer.parseInt(input.nextToken());
+    		System.out.println(num);
+    		sum=sum+num;
+    	}
+    	System.out.println("sum is "+ sum);
+    }
+}
