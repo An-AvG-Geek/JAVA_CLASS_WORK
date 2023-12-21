1b909f1d80cd286c9cdb7981ef8c15efca64e270
Felix Jobi <107713717+An-AvG-Geek@users.noreply.github.com>
2023-12-21 15:20:01 +0000

    Add files via upload

---
+++ Abstraction.java
@@ -0,0 +1,36 @@
+
+public class Abstraction {
+	public static void main(String [] args) {
+				Rectangle rectangle = new Rectangle();
+				Triangle triangle = new Triangle();
+				Hexagon hexagon = new Hexagon();
+				rectangle.numberOfSides();
+				triangle.numberOfSides();
+				hexagon.numberOfSides();
+	}
+}
+				
+		abstract class Shapes{
+		abstract public void numberOfSides();
+			
+		
+				
+			
+		}
+			class Rectangle extends Shapes{
+				public void numberOfSides(){
+					System.out.println("The number of sides of Rectangle is 4");
+				}
+			}
+			class Triangle extends Shapes{
+				public void numberOfSides() {
+					System.out.println("The number of sides of Triangle is 3");
+				}
+			}
+			class Hexagon extends Shapes{
+				public void numberOfSides() {
+					System.out.println("The number of sides of Hexagon is 6");
+				}
+			
+				
+	}
